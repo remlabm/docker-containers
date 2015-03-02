5bc9247021f8aa5781ef8b8c8d38131b40488ace
remlabm <remlabm@gmail.com>
2015-03-01 19:42:46 -0800

    moved range

--- openvpn-dnsmasq/service/dnsmasq.sh
+++ openvpn-dnsmasq/service/dnsmasq.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dnsmasq -u root -D -b -h -r /etc/resolv.conf -7 /etc/dnsmasq.d -a 127.0.0.1,192.168.255.1 -d
+dnsmasq -u root -D -b -h -r /etc/resolv.conf -7 /etc/dnsmasq.d -a 127.0.0.1,10.0.255.1 -d

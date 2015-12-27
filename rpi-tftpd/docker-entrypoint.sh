ab0e7f1d1702107c6af55b9855b78edba457ad74
remlabm <remlabm@gmail.com>
2015-12-26 17:00:20 -0800

    fixed yaml updated entrypoint

--- rpi-tftpd/docker-entrypoint.sh
+++ rpi-tftpd/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'tftp-server' ]; then
-	exec /usr/sbin/in.tftpd --foreground --ipv4 --user nobody --address 0.0.0.0:69 --secure /images "$@"
+	exec /usr/sbin/in.tftpd --foreground --ipv4 --user nobody --address 0.0.0.0:69 --secure /images
 fi
 
 exec "$@"
